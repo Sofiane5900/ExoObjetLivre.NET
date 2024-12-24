45dc57e3f48d7c9c1657f7566527a929a7be231d
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 09:31:07 +0100

    premiere partie exo fini

---
+++ ExoObjetLivre.NET/Livre.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoObjetLivre.NET
+{
+    internal class Livre
+    {
+        private string _titre;
+        private string _auteur;
+        private int _publication;
+        private bool _estEmprunter;
+
+
+        public string Titre { get => _titre;  set => _titre = value; }
+        public string Auteur { get => _auteur; set => _auteur = value; }
+        public int Publication { get => _publication; set => _publication = value; }
+        public bool estEmprunter {  get => _estEmprunter; set => _estEmprunter = value;}
+
+
+        public Livre(string Titre, string Auteur, int Publication, bool estEmprunter)
+        {
+            _titre = Titre;
+            _auteur = Auteur;
+            _publication = Publication;
+            _estEmprunter = estEmprunter;
+        }
+        public void Emprunter()
+        {
+            _estEmprunter = true;
+            Console.WriteLine("Disponible.");
+        }
+
+        public void Rendre()
+        {
+            estEmprunter = false;
+            Console.WriteLine("Indisponible.");
+        }
+
+        public void AfficherDetails()
+        {
+            // Opération ternaire pour choisir définir que false ou true = un string conrrespondant
+            string disponibilite = estEmprunter ? "indisponible" : "disponible";
+            Console.WriteLine($"Le livre {Titre}, écrit par {Auteur} en {Publication} est {disponibilite}");
+        }
+    }
+}
--- ExoObjetLivre.NET/Program.cs
+++ ExoObjetLivre.NET/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExoObjetLivre.NET;
+
+Livre harryPoter =  new Livre("Harry Poter", "J.K Rowling", 1997, true);
