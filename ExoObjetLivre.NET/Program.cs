57adf734d25a7af298882e5b6b308cf9ac34a1fc
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 09:46:48 +0100

    ajout liste et foreach

--- ExoObjetLivre.NET/Program.cs
+++ ExoObjetLivre.NET/Program.cs
@@ -3,10 +3,19 @@
 Livre harryPoter =  new Livre("Harry Poter", "J.K Rowling", 1997, true);
 Livre lesMisérables = new Livre("Les Misérables", "Victor Hugo", 1862, false);
 Livre leSeigneurDesAnneaux = new Livre("Le Seigneur des Anneaux", "J.R.R. Tolkien", 1954, true);
-List<Livre> livres = new List<Livre>();
-AfficherDetails(harryPoter);
-AfficherDetails(lesMisérables);
-AfficherDetails(leSeigneurDesAnneaux);
+List<Livre> listeLivres = new List<Livre>();
+listeLivres.Add(harryPoter);
+listeLivres.Add(lesMisérables);
+listeLivres.Add(leSeigneurDesAnneaux);
+
+foreach (Livre l in listeLivres)
+{
+    AfficherDetails(l);
+}
+
+
+//AfficherDetails(lesMisérables);
+//AfficherDetails(leSeigneurDesAnneaux);
 
 void AfficherDetails(Livre livre)
 {
